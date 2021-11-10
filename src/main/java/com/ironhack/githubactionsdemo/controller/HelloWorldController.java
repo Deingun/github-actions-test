097eb81810da36460dcbe371ce88cbcada6046bf
David de Ingunza <deingun@gmail.com>
2021-11-10 21:38:26 +0100

    initial commit

---
+++ src/main/java/com/ironhack/githubactionsdemo/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.ironhack.githubactionsdemo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    @ResponseStatus(HttpStatus.OK)
+    public String helloworld() {
+        return "Hello World";
+    }
+}
